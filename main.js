d72b180bdbcc005345d6ce69af7956f9c819c0a4
Colton Turner <colton_232@hotmail.com>
2022-08-03 17:31:41 -0700

    Added comments.

--- main.js
+++ main.js
@@ -2,9 +2,11 @@ const gridCon = document.querySelector('.grid-container');
 
 makeGrid(16, 16);
 
+//Creates a 16x16 grid
 function makeGrid(rows, columns) {
     gridCon.style.setProperty('--grid-rows', rows);
     gridCon.style.setProperty('--grid-columns', columns);
+    //Loops through and creates a new div grids
     for (i = 0; i < (rows * columns); i++) {
         let cell = document.createElement("div");
         gridCon.appendChild(cell).className = "grid-item";
